1a5521e7e78b3691e3ab49e2c006096b9c5d4475
Ian Wren <ian.wren81@gmail.com>
2021-01-25 12:49:26 -0800

    Cleaned server code and linked database name with called name in rest of code.

--- server.js
+++ server.js
@@ -17,87 +17,90 @@ const collections = ["workouts"];
 const db = mongojs(databaseUrl, collections);
 
 db.on("error", error => {
-  console.log("Database Error:", error);
+    console.log("Database Error:", error);
 });
 
 app.get("/", (req, res) => {
-  res.send(index.html);
+    res.send(index.html);
 });
 
 
 
-// Saves a note to the database's collection
+// Saves a workout to the database's collection
 // POST: /submit
 // ===========================================
 app.post("/submit", (req, res) => {
-  db.notes.insert(req.body, (err, saved) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(saved);
-    }
-  });
+    db.workouts.insert(req.body, (err, saved) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(saved);
+        }
+    });
 });
-// Retrieves all notes from the database's collection
+// Retrieves all workouts from the database's collection
 // GET: /all
 // ====================================================
 app.get("/all", (req, res) => {
-  db.notes.find({}, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
+    db.workouts.find({}, (err, found) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(found);
+        }
+    });
 });
-// 3. Retrieves one note in the database's collection by it's ObjectId
+// 3. Retrieves one workout in the database's collection by it's ObjectId
 // GET: /find/:id
 // ==================================================================
 app.get("/find/:id", (req, res) => {
-  const id = req.params.id
-  
-  db.notes.find(
-    _id = mongojs.ObjectId(id), (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
+    const id = req.params.id
+
+    db.workouts.find(
+        _id = mongojs.ObjectId(id), (err, found) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(found);
+            }
+        }
+
+    );
+
 });
-// 4. Updates one note in the database's collection by it's ObjectId
+// 4. Updates one workout in the database's collection by it's ObjectId
 // POST: /update/:id
 // ================================================================
 app.update("/update/:id", (req, res) => {
-  db.notes.insert({mongojs.ObjectId(req.params.id)}, {req.params}, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
+    db.workouts.insert({ mongojs.ObjectId(req.params.id) }, { req.params }, (err, found) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(found);
+        }
+    });
 });
-// 5. Deletes one note from the database's collection by it's ObjectId
+// 5. Deletes one workout from the database's collection by it's ObjectId
 // DELETE: /delete/:id
 // ==================================================================
 app.delete("/delete/:id", (req, res) => {
-  db.notes.remove({mongojs.ObjectId(req.id)}, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
+    db.workouts.remove({ mongojs.ObjectId(req.id) }, (err, found) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(found);
+        }
+    });
 });
-// 6. Clear the entire note collection
+// 6. Clear the entire workout collection
 // DELETE: /clearall
 // ===================================
 app.delete("/clearall", (req, res) => {
-  db.notes.remove({}, (err, found) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(found);
-    }
-  });
+    db.workouts.remove({}, (err, found) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(found);
+        }
+    });
 });
