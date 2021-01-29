d7041af13bbe22020b2062b3139fb6052384b0eb
Ian Wren <ian.wren81@gmail.com>
2021-01-28 16:39:13 -0800

    Added code to log server calls

--- server.js
+++ server.js
@@ -32,89 +32,10 @@ mongoose.connect(
     }
   );
 
-app.get("/", (req, res) => {
-    res.send(index.html);
-});
+  require("./routes/api-routes")(app);
 
 
-
-// Saves a workout to the database's collection
-// POST: /submit
-// ===========================================
-app.post("/submit", (req, res) => {
-    db.workouts.insert(req.body, (err, saved) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(saved);
-        }
-    });
-});
-// Retrieves all workouts from the database's collection
-// GET: /all
-// ====================================================
-app.get("/all", (req, res) => {
-    db.workouts.find({}, (err, found) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(found);
-        }
-    });
-});
-// 3. Retrieves one workout in the database's collection by it's ObjectId
-// GET: /find/:id
-// ==================================================================
-app.get("/find/:id", (req, res) => {
-    const id = req.params.id
-
-    db.workouts.find(
-        _id = mongojs.ObjectId(id), (err, found) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json(found);
-            }
-        }
-
-    );
-
-});
-// 4. Updates one workout in the database's collection by it's ObjectId
-// POST: /update/:id
-// ================================================================
-app.update("/update/:id", (req, res) => {
-    db.workouts.insert(
-        { _id=mongojs.ObjectId(req.params.id) }, { req.params}, (err, found) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(found);
-        }
-    });
-});
-// 5. Deletes one workout from the database's collection by it's ObjectId
-// DELETE: /delete/:id
-// ==================================================================
-app.delete("/delete/:id", (req, res) => {
-    db.workouts.remove({ _id=mongojs.ObjectId(req.id) }, (err, found) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(found);
-        }
-    });
-});
-// 6. Clear the entire workout collection
-// DELETE: /clearall
-// ===================================
-app.delete("/clearall", (req, res) => {
-    db.workouts.remove({}, (err, found) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.json(found);
-        }
-    });
-});
-
+  app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+      console.log("App running on port" + PORT);
+  });
