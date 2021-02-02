80b161ea29583008a327ad8a5b9e810b71ba26c0
Ian Wren <ian.wren81@gmail.com>
2021-02-01 16:46:02 -0800

    Added date to the database schema array

--- models/workout.js
+++ models/workout.js
@@ -2,29 +2,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Workout Name is Required"
+  day: {
+    type: Date,
+    default: Date.now
   },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Workout Name is Required"
+      },
 
-  type: {
-    type: String,
-    required: true
-  },
+      type: {
+        type: String,
+        required: true
+      },
 
-  weight: {
-    type: Number,
-  },
+      weight: {
+        type: Number,
+      },
 
-  sets: Number,
+      sets: Number,
 
-  reps: Number,
+      reps: Number,
 
-  duration: Number,
+      duration: Number,
 
-  distance: Number
-});
+      distance: Number
+    }]
+    });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
