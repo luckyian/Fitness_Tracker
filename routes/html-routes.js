08df3722b937803923ef310db1f3637359f8fa04
Ian Wren <ian.wren81@gmail.com>
2021-02-02 18:31:32 -0800

    Used router instead of app for better functionality

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,16 +1,18 @@
 const path = require("path");
+const router = require("express").Router();
 
-module.exports = (app) => {
+ 
 
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"))
-    });
+    // app.get("/", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../public/index.html"))
+    // });
 
-    app.get("/exercise", (req, res) => {
+    router.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"))
     });
 
-    app.get("/stats", (req, res) => {
+    router.get("/stats", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/stats.html"))
     });
-}
+
+    module.exports = router;
