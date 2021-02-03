c711741f4268220ece44c59ba3f5503e8313e05c
Ian Wren <ian.wren81@gmail.com>
2021-02-02 17:41:20 -0800

    Fixed aggregate errors and removed unnecessary curly braces

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,14 @@
-const db = require("./models");
+const db = require("../models");
 const router = require("express").Router();
+const mongojs = require("mongojs");
 // const databaseUrl = "workout";
 // const collections = ["workout"];
 
 // const db = mongojs(databaseUrl, collections);
 
-db.on("error", error => {
-    console.log("Database Error:", error);
-});
+// db.on("error", error => {
+//     console.log("Database Error:", error);
+// });
 
 router.get("/", (req, res) => {
     res.send(index.html);
@@ -30,7 +31,9 @@ router.post("/api/workouts", (req, res) => {
 // GET: /all
 // ====================================================
 router.get("/api/workouts", (req, res) => {
-    db.workout.aggregate([
+    db.collection('workout').aggregate
+    // db.workout.aggregate
+    ([
         {
             $addFields: {
                 totalDuration: { $sum: "$exercises.duration" },
@@ -62,20 +65,24 @@ router.get("/api/workouts/:id", (req, res) => {
 // POST: /update/:id
 // ================================================================
 router.put("/api/workouts/:id", (req, res) => {
-    db.workout.findByOneAndUpdate(
-        { _id=mongojs.ObjectId(req.params.id) }, { $push: { exercises: req.body } }, (err, found) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.json(found);
-            }
-        });
+    const id = req.params.id
+    db.workout.findByOneAndUpdate
+        (_id = mongojs.ObjectId(id),
+            { $push: { exercises: req.body } }, (err, found) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(found);
+                }
+
+            });
+
 });
 // 5. Deletes one workout from the database's collection by it's ObjectId
 // DELETE: /delete/:id
 // ==================================================================
 router.delete("/api/workouts/:id", (req, res) => {
-    db.workout.remove({ _id=mongojs.ObjectId(req.id) }, (err, found) => {
+    db.workout.remove(_id=mongojs.ObjectId(req.id) , (err, found) => {
         if (err) {
             console.log(err);
         } else {
