84d82ce646f382d0fe90d82ceffd8d9cde35dbd0
Ian Wren <ian.wren81@gmail.com>
2021-02-03 17:07:34 -0800

    Deleted unnecessary code.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,10 +3,7 @@ const router = require("express").Router();
 
  
 
-    // app.get("/", (req, res) => {
-    //     res.sendFile(path.join(__dirname, "../public/index.html"))
-    // });
-
+  
     router.get("/exercise", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/exercise.html"))
     });
